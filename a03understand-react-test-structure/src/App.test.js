e9131396edc66ad67c40f13ea0626adf93eb0886
sachin <sachinnarofficials@gmail.com>
2024-08-24 14:54:32 +0530

    testing input field

--- a03understand-react-test-structure/src/App.test.js
+++ a03understand-react-test-structure/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("text check", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const expText = screen.getByText(/code step by step/i);
+  expect(expText).toBeInTheDocument();
 });
--- a06input-box-test/src/App.js
+++ a06input-box-test/src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          placeholder="Enter User Name"
+          name-="username"
+          id="userId"
+        />
         <p>Code Step by Step</p>
       </header>
     </div>
